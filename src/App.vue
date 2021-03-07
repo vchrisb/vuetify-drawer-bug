67c206628064ad055ccf794580d93abec7b10ee5
vchrisb <vchrisb@users.noreply.github.com>
2021-03-07 22:58:23 +0100

    repo

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    
+    <v-navigation-drawer dark class="deep-purple accent-4" v-model="menu" clipped temporary app bottom>
+      <v-btn @click="menu = !menu">
+        Close
+      </v-btn>
+            <v-list>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          link
+          class="red"
+        >
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-main>
+      <v-btn @click="menu = !menu">
+        Drawer
+      </v-btn>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+
+  components: {
+  },
+
+  data: () => ({
+    menu: false,
+    items: [
+          { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+          { title: 'Account', icon: 'mdi-account-box' },
+          { title: 'Admin', icon: 'mdi-gavel' },
+        ],
+  }),
+};
+</script>
---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
+  pwa: {
+    name: 'Drawer',
+    themeColor: '#d8be00',
+    msTileColor: '#d8be00',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'black-translucent',
+  }
+}
